2d2d85d808a80088773ee8ac62967616d10ec40e
Priyanshu Sharma <95965636+PriyanshuSharma0222@users.noreply.github.com>
2023-07-14 04:22:13 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         const conn = await mongoose.connect(`${process.env.MONGO_URL}`);
-        // console.log(`<<< [db.js] >>> SUCCESSFULLY CONNECTED TO DATABASE : ${conn.connection.host} >>>`);
+        console.log(`<<< [db.js] >>> SUCCESSFULLY CONNECTED TO DATABASE : ${conn.connection.host} >>>`);
     
     } catch (error) {
         console.error(`<<< [db.js] >>> FAILED TO CONNECT TO DATABASE : ${error} >>>`);
